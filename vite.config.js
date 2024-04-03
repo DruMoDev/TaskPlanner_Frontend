288d9be8e7d98c9f0760b4f3b7d88fc63356ed59
DruMoDev <dev.morera@gmail.com>
2024-04-03 14:26:51 +0200

    Configuracion .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+
+// Cargar variables de entorno dependiendo de NODE_ENV
+const envFile = process.env.NODE_ENV === 'production' ? 'production.env' : 'development.env';
+dotenv.config({ path: envFile });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
